b33c7864bbbf4799de86ee96e9b704a9bc0f7d7c
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-04-10 08:49:28 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { BsFillSquareFill } from 'react-icons/bs';
+import { BsCircle } from 'react-icons/bs';
+import './App.css'
 
-function App() {
+const App = () => {
+    const [shape,setShape] = useState(<BsFillSquareFill />)
+    let changeShape = () => {
+      setShape( <BsCircle />)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Changes text on click</h1>
+                <br/>
+                <h3>{shape}</h3>
+                <br/>
+                <button onClick={changeShape}>Click me!</button>
+                <br/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
